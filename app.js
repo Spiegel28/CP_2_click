8107f0ff4f4ee2eb9fbd30533aa20ab45c9bcb51
Alex Spiegel <alexspiegel28@gmail.com>
2024-02-04 21:10:35 -0700

    working pn auto

---
+++ app.js
@@ -0,0 +1,126 @@
+let cheese = 0;
+let clicks = 0;
+
+let clickUpgrades = [
+    {
+      name: 'knife',
+      price: 1,
+      quantity: 0,
+      multiplier: 2
+    },
+
+    {
+        
+        name: 'drill',
+        price: 150,
+        quantity: 0,
+        multiplier: 10
+    }
+  ];
+  
+  let automaticUpgrades = [
+    {
+      name: 'wormhole',
+      price: 600,
+      quantity:0,
+      multiplier: 20
+    },
+
+    {
+      name: 'tom cruise',
+      price: 1200,
+      quantity: 0,
+      multiplier: 50
+    }
+  ];
+
+  function buyKnife() {
+    const knife = clickUpgrades[0];
+    if (cheese >= knife.price) {
+      cheese -= knife.price;
+      knife.quantity++;
+      console.log("Purchased");
+      update();
+    }
+      else {
+        window.alert("NO CHEESE");
+      }
+    }
+// const drill = clickUpgrades.find(drill => drill.name === 'drill')
+function buyDrill() {
+    const drill = clickUpgrades.find(drill => drill.name === 'drill')
+    if (cheese >= drill.price) {
+      cheese -= drill.price;
+      drill.quantity++;
+      console.log("Purchased");
+      update();
+    }
+      else {
+        window.alert("NO CHEESE");
+      }
+    }
+  
+function applyClickUpgrades() {
+    const knife = clickUpgrades.find(knife => knife.name === 'knife')
+    cheese += knife.quantity * knife.multiplier;
+    update();
+
+}
+
+function applyDrillUpgrade() {
+    const drill = clickUpgrades.find(drill => drill.name === 'drill')
+    cheese += drill.quantity * drill.multiplier
+    update()
+}
+
+function collectAutoUpgrades() {
+        const automaticUpgradesToCollect = automaticUpgrades.filter(upgrade => upgrade.quantity > 0);
+      
+        
+        const totalAutoResources = automaticUpgradesToCollect.reduce((total, upgrade) => {
+          return total + (upgrade.quantity * upgrade.multiplier);
+        }, 0);
+      
+        
+        cheese += totalAutoResources;
+        update();
+      }
+
+
+function mine() {
+    cheese++ ;
+    clickCount()
+    applyClickUpgrades()
+    applyDrillUpgrade()
+    update()
+} 
+function clickCount() {
+clicks++
+// ANCHOR figure out why it starts at 2 clicks
+}
+
+function update() {
+    const cheeseCountElement = document.getElementById('cheeseCount');
+    cheeseCountElement.textContent = `${cheese} 🧀`;
+
+    const clickCountElement = document.getElementById('clickCount');
+    clickCountElement.textContent = `${clicks} 👆🏼`;
+    // const knifeCountElement = document.getElementById('knifeCount');
+    // knifeCountElement.textContent = `${clickUpgrades[0].quantity} Knife`;
+    const knifeCountElement = document.getElementById('knifeCount');
+    knifeCountElement.textContent = `${clickUpgrades.find(knife => knife.name === 'knife').quantity} knife`;
+
+    const drillCountElement = document.getElementById('drillCount');
+    drillCountElement.textContent = `${clickUpgrades.find(drill => drill.name === 'drill').quantity} drill`;
+
+    // const wormholeCountElement = document.getElementById('wormholeCount');
+    // wormholeCountElement.textContent = `${automaticUpgrades.find(wormhole => wormhole.name === 'wormhole').quantity} wormhole`;
+
+    // const tomcruiseCountElement = document.getElementById('tomcruiseCount');
+    // tomcruiseCountElement.textContent = `${automaticUpgrades.find(tomcruise => tomcruise.name === 'tomcruise').quantity} tomcruise`;
+
+  }
+  
+setInterval(collectAutoUpgrades, 3000);
+clickCount()
+mine()
